da278594ae7ebc93fe5d59530314fe1aa7a8dd4c
ThisisNirmalJaiswal <105931254+ThisisNirmalJaiswal@users.noreply.github.com>
2022-09-06 14:53:26 +0530

    Update blogController.js

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -102,7 +102,7 @@ const deleteblog = async function (req,res){
   }
   
 }
-//__________________________________Get Blog____________________________________
+//__________________________________Moduels____________________________________
 
 
 module.exports.blogcreate = blogcreate;
